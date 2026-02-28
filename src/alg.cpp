d7f2649a697387e42376548b1757bfa874273597
Рыбина Екатерина <rybinae58@gmail.com>
2026-02-28 22:47:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,46 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
-  return true;
+  bool result = true;
+  if (value <= 1) return false;
+  else if (value <= 3) return true;
+  for (uint64_t i = 2; i < value; i++){
+    if (value % 2 == 0){
+      result = false;
+      break;
+    }
+  }
+  if (result == true) return true;
+  else return false;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  uint64_t count = 0, a = 1;
+  while (count < a) {
+    a++;
+    if(checkPrime(a)) count++;
+  }
+  return a;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t k = value + 1;
+  if (k % 2 == 0 && k != 2) ++k;
+  while (!chekPrime(k))
+    k += (k == 2) ? 1 : 2;
+  return k;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t s = 2;
+  for (uint64_t i = 3; i < hbound; i += 2)
+    if (chekPrime(s)) s += i;
+  return s;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-  // вставьте код функции
-  return 1;
+  uint64_t cnt_pair = 0;
+  for (uint64_t i = lbound; i < hbound - 2; ++i)
+    if (chekPrime(i) && chekPrime(i + 2)) ++cnt_pair;
+  return cnt_pair;
 }

06689c71bd6deb6a0a0f00b787fb9037f549b53b
Рыбина Екатерина <rybinae58@gmail.com>
2026-05-19 22:51:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 #include "alg.h"
 
 
-bool checkPrime(uint64_t value) { 
+bool checkPrime(uint64_t value) {
    bool result = true;
   if (value <= 1) return false;
   else if (value <= 3) return true;
@@ -30,21 +30,21 @@ uint64_t nextPrime(uint64_t value) {
   uint64_t k = value + 1;
   if (k % 2 == 0 && k != 2) ++k;
   while (!checkPrime(k)) {
-        k += (k == 2) ? 1 : 2; 
+        k += (k == 2) ? 1 : 2;
     }
   return k;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t s = 2; 
-  for (uint64_t i = 3; i < hbound; i += 2) 
+  uint64_t s = 2;
+  for (uint64_t i = 3; i < hbound; i += 2)
         if (checkPrime(i)) s += i;
   return s;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t cnt_pair = 0;
-  for (uint64_t i = lbound; i < hbound - 2; ++i) 
+  for (uint64_t i = lbound; i < hbound - 2; ++i)
         if (checkPrime(i) && checkPrime(i + 2)) ++cnt_pair;
   return cnt_pair;
 }
